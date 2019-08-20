57bade8992c33e399d401c051223681b8e43f444
Meitar Cohen <meitarc@gmail.com>
2019-08-20 19:41:44 +0300

    add auth moduls and lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
-import { LoginComponent } from './auth/login/login.component';
-import { SignUpComponent } from './auth/signup/signup.component';
 import { AuthInterceptor } from './auth/auth-interceptor';
 import { ErrorInterceptor } from './error-intreceptor';
 import { ErrorComponent } from './error/error.component';
@@ -19,14 +16,11 @@ import { PostsModule } from './posts/posts.module';
   declarations: [
     AppComponent,
     HeaderComponent,
-    LoginComponent,
-    SignUpComponent,
     ErrorComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
     BrowserAnimationsModule,
     HttpClientModule,
     AngularMaterialModule,
