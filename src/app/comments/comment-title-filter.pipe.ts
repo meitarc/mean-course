e6585022d6c3af89cd4b22f0076304cc50ed3222
Boris Binaev <borisbinaev@gmail.com>
2019-08-25 17:01:38 +0300

    updateSearchComment

---
+++ src/app/comments/comment-title-filter.pipe.ts
@@ -0,0 +1,17 @@
+import{Comment}from './comment.model';
+import {PipeTransform,Pipe} from '@angular/core';
+
+@Pipe({
+    name:'commentTitleFilter'
+})
+
+export class CommentTitleFilterPipe implements PipeTransform{
+    transform(comments:Comment[],searchTerm:String):Comment[] {
+        if(!comments || !searchTerm)
+        {
+            return comments;
+        }
+        return comments.filter(comment=>
+            comment.title.toLocaleLowerCase().indexOf(searchTerm.toLocaleLowerCase())!== -1);
+    }
+}
--- src/app/comments/comments.module.ts
+++ src/app/comments/comments.module.ts
@@ -1,21 +1,26 @@
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { CommentCreateComponent } from './comment-create/comment-create.component';
 import { CommentListComponent } from './comment-list/comment-list.component';
 import { AngularMaterialModule } from '../angular-material.module';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { CommentContentFilterPipe } from './comment-content-filter.pipe';
+import { CommentTitleFilterPipe } from './comment-title-filter.pipe';
 
 
 @NgModule({
   declarations: [
     CommentCreateComponent,
-    CommentListComponent
+    CommentListComponent,
+    CommentContentFilterPipe,
+    CommentTitleFilterPipe
   ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    FormsModule,
     AngularMaterialModule,
     RouterModule
   ]
