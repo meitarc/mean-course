afa86a24b107cea2789da74244c61988c14e5ba0
Guy <ohayonguy1@gmail.com>
2019-08-26 15:01:17 +0300

    updated the scraper. still need to export it as a function correctly, had to re-write it.

--- src/app/scraping/scrap.js
+++ src/app/scraping/scrap.js
@@ -1,6 +1,6 @@
-var request = require('request');
-var cheerio = require('cheerio');
-var fs = require('fs');
+request = require('request');
+cheerio = require('cheerio');
+fs = require('fs');
 
 request("https://news.ycombinator.com/news", function(error, response, body) {
   if(error) {
@@ -8,11 +8,11 @@ request("https://news.ycombinator.com/news", function(error, response, body) {
   }
   console.log("Status code: " + response.statusCode);
 
-  var $ = cheerio.load(body);
+  $ = cheerio.load(body);
 
   $('tr.athing:has(td.votelinks)').each(function( index ) {
-    var title = $(this).find('td.title > a').text().trim();
-    var link = $(this).find('td.title > a').attr('href');
+    title = $(this).find('td.title > a').text().trim();
+    link = $(this).find('td.title > a').attr('href');
     fs.appendFileSync('hackernews.txt', body);
   });
 
