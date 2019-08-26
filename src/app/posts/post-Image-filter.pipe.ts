ab3c19411c67efeacf375960a230080b358d80c5
Meitar Cohen <meitarc@gmail.com>
2019-08-26 18:40:38 +0300

    add "include post" query

--- src/app/posts/post-Image-filter.pipe.ts
+++ src/app/posts/post-Image-filter.pipe.ts
@@ -6,11 +6,22 @@ import { PipeTransform , Pipe } from '@angular/core';
 })
 
 export class postImageFilterPipe implements PipeTransform {
-    transform(posts: Post[], Image: boolean): Post[] {
+    postsToReturn: Post[] = [];
 
-      if (!posts || !Image) {
-            return posts;
+    transform(posts: Post[], Image: boolean): Post[] {
+      this.postsToReturn = [];
+
+      for (const post of posts) {
+        if (post.imagePath != null) {
+          console.log('post to return ' + this.postsToReturn);
+          console.log(post);
+
+          this.postsToReturn.push(post);
         }
-      return null;
+      }
+      if (!posts || !Image) {
+            return this.postsToReturn;
+      }
+      return posts;
     }
 }
